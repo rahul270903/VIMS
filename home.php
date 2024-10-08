1a28728789f01ca153a71780846f7ba7144534cb
rahul270903 <rahulborkar27karwar@gmail.com>
2024-10-08 23:08:17 +0530

    init

---
+++ home.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>XAMAPP(2024)</h1>
+    <p></p>
+    <?php
+    echo "<p>Hello World from PHP</p>\n";
+    echo "date: " . date('Y-m-d H:i:s');
+    ?>
+    </body>
+</html>
